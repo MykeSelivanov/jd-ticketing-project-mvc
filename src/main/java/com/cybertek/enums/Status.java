325d3d652e13a8a2e59e3977fa2eccf7d4cd61e8
MykeSelivanov <michael777selivanov@gmail.com>
2020-11-28 15:03:19 -0600

    Ticketing project MVC

--- src/main/java/com/cybertek/enums/Status.java
+++ src/main/java/com/cybertek/enums/Status.java
@@ -2,7 +2,7 @@ package com.cybertek.enums;
 
 public enum Status {
 
-    OPEN("Open"), IN_PROGRESS("In Progress"), COMPLETE("COmpleted"), UAT_TEST("UAT Testing");
+    OPEN("Open"), IN_PROGRESS("In Progress"), COMPLETE("Completed"), UAT_TEST("UAT Testing");
 
     private final String value;
 
